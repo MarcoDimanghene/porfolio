7986ad5af76b55d4444e284c384f8986c2eb58fb
Marco <Dimanghenemarco@gmail.com>
2024-11-28 19:17:43 -0300

    no anda

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { useThemeToggle } from "./Utils/constants";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/home/home";
 import ProjectsPage from "./pages/Projects/ProjectsPage";
-import { Skillpage } from "./pages/SkillPage/Skillpage";
-import { EduPage } from "./pages/Education/EduPage";
+import  Skillpage  from "./pages/SkillPage/Skillpage";
+import  EduPage  from "./pages/Education/EduPage";
 
 
 function App() {
